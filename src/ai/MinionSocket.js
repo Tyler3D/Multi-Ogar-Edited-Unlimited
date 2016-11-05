21a29fee47965d28e1b008fd84ca497239f6b289
Megabyte918 <cisr918@gmail.com>
2016-11-05 16:52:37 -0400

    Minions

---
+++ src/ai/MinionSocket.js
@@ -0,0 +1,17 @@
+// A fake socket for minions
+
+function MinionSocket(gameServer) {
+  this.isCloseRequested = false;
+}
+
+module.exports = MinionSocket;
+
+MinionSocket.prototype.sendPacket = function (packet) {
+    // Fakes sending a packet
+    return;
+};
+
+MinionSocket.prototype.close = function (error) {
+    // Removes the minion
+    this.isCloseRequest = true;
+};
