c6505f7780572197ca78f29f1764727fa52abbf3
Megabyte918 <cisr918@gmail.com>
2016-11-06 20:55:19 -0500

    Update MinionPlayer.js

--- src/ai/MinionPlayer.js
+++ src/ai/MinionPlayer.js
@@ -3,23 +3,12 @@ var PlayerTracker = require('../PlayerTracker');
 function MinionPlayer() {
     PlayerTracker.apply(this, Array.prototype.slice.call(arguments));
     this.isMi = true; // Marks as minion
-    this.splitCooldown = 0;
 }
 
 module.exports = MinionPlayer;
 MinionPlayer.prototype = new PlayerTracker();
 
 // Functions
-MinionPlayer.prototype.getLowestCell = function () {
-    // Sort the cells by Array.sort() function to avoid errors
-    var sorted = this.cells.valueOf();
-    sorted.sort(function (a, b) {
-        return b.getSize() - a.getSize();
-    });
-    
-    return sorted[0];
-};
-
 MinionPlayer.prototype.checkConnection = function () {
     if (this.socket.isCloseRequest) {
         while (this.cells.length > 0) {
@@ -45,19 +34,10 @@ MinionPlayer.prototype.checkConnection = function () {
 
 // Overrides the update function from player tracker
 MinionPlayer.prototype.sendUpdate = function () { 
-    if (this.splitCooldown > 0) this.splitCooldown--;
-    var cell = this.getLowestCell();
-    this.decide(cell);
-};
-
-MinionPlayer.prototype.decide = function (cell) {
-    if (!cell) return; // Cell was eaten, check in the next tick
-    
     // Since this is for minions, just follow the players mouse
-    var player = this.gameServer.minionLeader.mouse;
     if (!this.gameServer.minionLeader.isRemoved) {
-        this.mouse = player;
+        this.mouse = this.gameServer.minionLeader.mouse;
     } else {
-        this.mouse = player;
+        this.mouse = this.gameServer.minionLeader.mouse;
     }
 };
