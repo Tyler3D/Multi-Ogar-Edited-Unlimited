49c8c960fcfc2c89821dc9900a018083f1ccd175
Megabyte918 <cisr918@gmail.com>
2016-11-08 03:41:07 -0500

    Update Cell.js
    Balances out with normalizedDistance

--- src/entity/Cell.js
+++ src/entity/Cell.js
@@ -84,7 +84,7 @@ Cell.prototype.getSpeed = function () {
         // tickStep = 40ms
         this._speed = (this.owner.customspeed > 0) ? 
         speed * 40 * this.owner.customspeed : // Set by command
-        speed * 40 * this.gameServer.config.playerSpeed;
+        speed * 40 * this.gameServer.config.playerSpeed / 2;
     }
     return this._speed;
 };
