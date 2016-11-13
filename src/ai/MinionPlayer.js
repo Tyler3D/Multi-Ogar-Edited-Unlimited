1efa996ce68211c36d72f345b2826f02fd639a91
Megabyte918 <cisr918@gmail.com>
2016-11-12 23:25:20 -0500

    Update MinionPlayer.js

--- src/ai/MinionPlayer.js
+++ src/ai/MinionPlayer.js
@@ -2,13 +2,12 @@ var PlayerTracker = require('../PlayerTracker');
 
 function MinionPlayer() {
     PlayerTracker.apply(this, Array.prototype.slice.call(arguments));
-    this.isMi = true; // Marks as minion
+    this.isMi = true;   // Marks as minion
 }
 
 module.exports = MinionPlayer;
 MinionPlayer.prototype = new PlayerTracker();
 
-// Functions
 MinionPlayer.prototype.checkConnection = function () {
     if (this.socket.isCloseRequest) {
         while (this.cells.length > 0) {
@@ -17,23 +16,14 @@ MinionPlayer.prototype.checkConnection = function () {
         this.isRemoved = true;
         return;
     }
-    
-    // Respawn if minion is dead
     if (this.cells.length <= 0) {
         this.gameServer.gameMode.onPlayerSpawn(this.gameServer, this);
-        if (this.cells.length == 0) {
-            // If the minion cannot spawn any cells, then disconnect it
-            this.socket.close();
-        }
+        if (this.cells.length == 0) this.socket.close();
     }
-    if (!this.gameServer.minionLeader.socket.isConnected || this.gameServer.minionEnabled == false) {
+    if (!this.owner.socket.isConnected || !this.owner.minionControl) 
         this.socket.close();
-    }
 };
 
-// Overrides the update function from player tracker
 MinionPlayer.prototype.sendUpdate = function () { 
-    // Since this is for minions, just follow the players mouse
-    var m = this.gameServer.minionLeader.mouse;
-    this.mouse = !this.gameServer.minionLeader.isRemoved ? m : m;
+    this.mouse = this.owner.mouse;
 };
