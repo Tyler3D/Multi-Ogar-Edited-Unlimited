b03db15bfe91599a9eea8b4e265b04abdb8d0c57
Megabyte918 <cisr918@gmail.com>
2016-11-27 17:47:06 -0500

    Delete Install Dependencies.bat
    moved to /run

--- Install Dependencies.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-npm install
-echo.
-echo.
-echo Successfully installed all dependecies!
-echo Please don't touch folder "node-modules".
-echo It is the folder where dependecies are installed.
-pause >nul
