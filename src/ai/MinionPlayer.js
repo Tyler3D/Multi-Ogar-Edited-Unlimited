db8bbbfeb8aeb655b6a0418fb4b769de8360039c
ZfsrGhS953 <j1355444@trbvm.com>
2016-11-28 19:03:51 +0300

    Update MinionPlayer.js

--- src/ai/MinionPlayer.js
+++ src/ai/MinionPlayer.js
@@ -22,5 +22,31 @@ MinionPlayer.prototype.checkConnection = function () {
     }
     if (!this.owner.socket.isConnected || !this.owner.minionControl) 
         this.socket.close();
-    this.mouse = this.owner.mouse;
+	if(this.owner.minionCollectPellets){
+		if (this.gameServer.tickCounter % 10 == 0) {
+		this.updateCenterInGame();
+		this.updateViewBox();
+		this.viewNodes = [];
+		var self = this;
+		this.gameServer.quadTree.find(this.viewBox, function (quadItem) {
+            if (quadItem.cell.cellType == 1)
+                self.viewNodes.push(quadItem.cell);
+        });
+		var bestDistance = 1e999;
+		for (var i in this.viewNodes) {
+			var cell = this.viewNodes[i];
+			var dx = this.cells[0].position.x - cell.position.x;
+			var dy = this.cells[0].position.y - cell.position.y;
+			var distance = dx * dx + dy * dy;
+			if (distance < bestDistance) {
+			bestDistance = distance;
+			this.mouse.x = cell.position.x;
+			this.mouse.y = cell.position.y;
+			}
+		}
+		}
+	} else {
+		this.mouse.x = this.owner.mouse.x;
+	    this.mouse.y = this.owner.mouse.y;
+	}
 };
