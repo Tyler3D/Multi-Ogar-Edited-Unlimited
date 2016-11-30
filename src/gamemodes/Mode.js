494281d6bd10529190fa2bbb21e2e1a1d42aaeab
Megabyte918 <cisr918@gmail.com>
2016-11-29 19:12:57 -0500

    Update Mode.js

--- src/gamemodes/Mode.js
+++ src/gamemodes/Mode.js
@@ -1,10 +1,9 @@
-﻿function Mode() {
+function Mode() {
     this.ID = -1;
     this.name = "Blank";
     this.decayMod = 1.0; // Modifier for decay rate (Multiplier)
     this.packetLB = 49; // Packet id for leaderboard packet (48 = Text List, 49 = List, 50 = Pie chart)
     this.haveTeams = false; // True = gamemode uses teams, false = gamemode doesnt use teams
-    
     this.specByLeaderboard = false; // false = spectate from player list instead of leaderboard
 }
 
@@ -44,6 +43,6 @@ Mode.prototype.onCellMove = function (cell, gameServer) {
 };
 
 Mode.prototype.updateLB = function (gameServer) {
-    gameServer.leaderboardType = this.packetLB;
     // Called when the leaderboard update function is called
+    gameServer.leaderboardType = this.packetLB;
 };
