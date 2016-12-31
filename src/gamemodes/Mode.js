fa429ecb33ba819c703a5bbcbc8759e277955710
Tyler3D <Tylermanrique@gmail.com>
2016-12-31 13:06:58 -0500

    Update Mode.js

--- src/gamemodes/Mode.js
+++ src/gamemodes/Mode.js
@@ -5,6 +5,7 @@ function Mode() {
     this.packetLB = 49; // Packet id for leaderboard packet (48 = Text List, 49 = List, 50 = Pie chart)
     this.haveTeams = false; // True = gamemode uses teams, false = gamemode doesnt use teams
     this.specByLeaderboard = false; // false = spectate from player list instead of leaderboard
+    this.lastmanStanding = false;
 }
 
 module.exports = Mode;
@@ -14,7 +15,25 @@ module.exports = Mode;
 Mode.prototype.onServerInit = function (gameServer) {
     // Called when the server starts
     gameServer.run = true;
+       if (gameServer.config.lastManStanding == 1) {
+        var short = 15 * 60000; // 15 Minutes
+        var long = 60 * 60000; // 60 Minutes
+        var time = Math.floor((Math.random() * (long - short)) + short);
+        var shortreset = 15;
+        var longreset = 30;
+        var resetTime = (Math.floor((Math.random() * (longreset - shortreset)) + shortreset)) + time;
+        var startInt = setInterval(function() {self.lastManStandingstart()}, time);
+        var endInt = setInterval(function() {self.lastManStandingend()}, resetTime);
+     }
 };
+
+Mode.prototype.lastManStandingstart = function(gameServer) {
+     this.lastManStandingStart = true;
+ };
+ 
+ Mode.prototype.lastManStandingend = function(gameServer) {
+     this.lastManStandingStart = false;
+ };
 
 Mode.prototype.onTick = function (gameServer) {
     // Called on every game tick 
@@ -26,9 +45,11 @@ Mode.prototype.onPlayerInit = function (player) {
 
 Mode.prototype.onPlayerSpawn = function (gameServer, player) {
     // Called when a player is spawned
+    if (!this.lastmanstanding){
     player.setColor(gameServer.getRandomColor()); // Random color
     gameServer.spawnPlayer(player, gameServer.randomPos());
-};
+    }
+    };
 
 Mode.prototype.onCellAdd = function (cell) {
     // Called when a player cell is added
