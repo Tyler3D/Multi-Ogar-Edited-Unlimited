1144cc0debb907e1fd9079f6b33f5c6eb0cb64ee
Tyler3D <Tylermanrique@gmail.com>
2017-01-04 19:52:35 -0500

    Update index.js

--- src/entity/index.js
+++ src/entity/index.js
@@ -1,8 +1,9 @@
-﻿module.exports = {
+module.exports = {
     Cell: require('./Cell'),
     PlayerCell: require('./PlayerCell'),
     Food: require('./Food'),
     Virus: require('./Virus'),
     MotherCell: require('./MotherCell'),
+    PopsplitVirus: require('./PopsplitVirus'),
     EjectedMass: require('./EjectedMass'),
 };
