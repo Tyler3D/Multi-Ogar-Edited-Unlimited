566d675cfb229379b8407d69dcef3d2fada1910f
Tyler3D <Tylermanrique@gmail.com>
2017-02-11 18:22:44 -0500

    Update GameServer.js

--- src/GameServer.js
+++ src/GameServer.js
@@ -126,7 +126,8 @@ function GameServer() {
         serverMinions: 0,           // Amount of minions each player gets once they spawn
         defaultName: "minion",      // Default name for all minions if name is not specified using command
         collectPellets: 0,          // Enable collect pellets mode. To use just press P or Q. (Warning: this disables Q controls, so make sure that disableERT is 0)
-    	slithermode: 0,             //Enable Slither Mode. To use just press Q. (Warning: Change Disable Q to 1 or this will not work)
+    	slithermode: 0,             // Enable Slither Mode. To use just press Q. (Warning: Change Disable Q to 1 or this will not work)
+        leaderboardmass: 0,         // Shows your mass on the leaderboard. (Warning: The leaderboard may become crowded because of this)
     };
     this.ipBanList = [];
     this.minionTest = [];
