87eef7cabffa7915d78faa723e5923fd5673efec
Tyler3D <Tylermanrique@gmail.com>
2017-03-03 22:44:11 -0500

    Update UserRoleEnum.js

--- src/enum/UserRoleEnum.js
+++ src/enum/UserRoleEnum.js
@@ -1,4 +1,4 @@
-﻿function define(name, value) {
+function define(name, value) {
     Object.defineProperty(exports, name, {
         value: value,
         enumerable: true,
@@ -11,3 +11,6 @@ define("GUEST", 0);
 define("USER", 1);
 define("MODER", 2);
 define("ADMIN", 4);
+define("1", 1);
+define("2", 2);
+define("4" , 4);
