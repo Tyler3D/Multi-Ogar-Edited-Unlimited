fd1c8b57e99ed30d84f79214792120f29c68434b
Tyler3D <Tylermanrique@gmail.com>
2017-03-06 15:23:48 -0500

    Update PlayerCommand.js
    Fixes #57

--- src/modules/PlayerCommand.js
+++ src/modules/PlayerCommand.js
@@ -681,6 +681,7 @@ var playerCommands = {
             return;
         }
         var PlayerTracker = require("../PlayerTracker");
+        var Packet = require("../Packet");
         PlayerTracker.prototype.joinGame = function (name, skin) {
 	for (var i in this.levelexps) {
 		if (this.levelexps[i + 1] < this.level)
